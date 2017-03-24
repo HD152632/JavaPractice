b80702409f23d53ad94f0f7e4966728ccf6dd60a
cuizz <cuizz@DESKTOP-Q1ORL3S>
2017-03-24 09:09:41 +0900

    과제보냄 car

---
+++ src/dimigo/org/oop/Car.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+package dimigo.org.oop;
+
+/**
+ * <pre>
+ * dimigo.org.oop
+ * └cuizz
+ *
+ * 1.what:
+ * 2.date: 2017. 3. 23.
+ * </pre>
+ * @author: cuizz
+ * @versio: 1.0
+ */
+public class Car {
+	String company,model,color;
+	public String getCompany() {
+		return company;
+	}
+	public void setCompany(String company) {
+		this.company = company;
+	}
+	public String getModel() {
+		return model;
+	}
+	public void setModel(String model) {
+		this.model = model;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public int getMaxSpeed() {
+		return maxSpeed;
+	}
+	public void setMaxSpeed(int maxSpeed) {
+		this.maxSpeed = maxSpeed;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	int maxSpeed,price;
+	
+	
+}
